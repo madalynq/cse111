3fd802d3ca0ffefd25945d7348cbe09e3da80922
madalynq <iwa.maddy@gmail.com>
2025-03-06 15:42:09 -0700

    cleaning up code

--- tire_volume.py
+++ tire_volume.py
@@ -4,8 +4,6 @@ width = int(input("Enter the width of the tire in mm (ex 205): "))
 ratio = int(input("Enter the aspect ratio of the tire (ex 60): "))
 diameter = int(input("Enter the diameter of the wheel in inches (ex 15): "))
 
-# ratio /= 100
-
 volume = (math.pi * width ** 2 * ratio * (width * ratio + 2540 * diameter)) / 10000000000
 
 print(f"The approximate volume is {volume:.2f} liters.")
